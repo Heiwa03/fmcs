f6ffb41b7b876a3cc32ca09a16eb5e51b89974ee
Vlad Rusu <rusu.v2k@gmail.com>
2024-11-12 19:13:06 +0200

    Fix idiotic handling of the message from string to byte array

--- Hashsing.cs
+++ Hashsing.cs
@@ -10,31 +10,25 @@ namespace FMCS
         private const uint C = 0x98badcfe;
         private const uint D = 0x10325476;
 
-        private static string PadMessage(string message)
+        private static byte[] PadMessage(byte[] message)
         {
-            // Append a 1 to the message
-            message += "1";
-
-            // Append 0s to the message until the length is 448 mod 512
-            while (message.Length % 512 != 448)
-            {
-                message += "0";
-            }
-
-            // Append the length of the message in bits as a 64-bit number
-            message += Convert.ToString(message.Length * 8, 2).PadLeft(64, '0');
-
-            return message;
+            int originalLength = message.Length;
+            int paddingLength = (56 - (originalLength + 1) % 64) % 64;
+            byte[] paddedMessage = new byte[originalLength + paddingLength + 9];
+            Array.Copy(message, paddedMessage, originalLength);
+            paddedMessage[originalLength] = 0x80;
+            ulong messageLengthBits = (ulong)originalLength * 8;
+            Array.Copy(BitConverter.GetBytes(messageLengthBits), 0, paddedMessage, paddedMessage.Length - 8, 8);
+            return paddedMessage;
         }
-        private static uint[] BreakMessageIntoBlocks(string message)
+
+        private static uint[] BreakMessageIntoBlocks(byte[] message)
         {
-            uint[] blocks = new uint[message.Length / 512];
-
-            for (int i = 0; i < message.Length; i += 512)
+            uint[] blocks = new uint[message.Length / 4];
+            for (int i = 0; i < blocks.Length; i++)
             {
-                blocks[i / 512] = Convert.ToUInt32(message.Substring(i, 512), 2);
+                blocks[i] = BitConverter.ToUInt32(message, i * 4);
             }
-
             return blocks;
         }
 
@@ -66,7 +60,7 @@ namespace FMCS
         public static string Hash(string input)
         {
             // Step 1: Pad the message
-            string paddedMessage = PadMessage(input);
+            byte[] paddedMessage = PadMessage(Encoding.UTF8.GetBytes(input));
 
             // Step 2: Break the message into 512-bit blocks
             uint[] blocks = BreakMessageIntoBlocks(paddedMessage);
@@ -85,13 +79,10 @@ namespace FMCS
             }
 
             // Step 4: Process each block
-            foreach (uint block in blocks)
+            for (int i = 0; i < blocks.Length / 16; i++)
             {
                 uint[] X = new uint[16];
-                for (int i = 0; i < 16; i++)
-                {
-                    X[i] = (block >> (i * 32)) & 0xFFFFFFFF;
-                }
+                Array.Copy(blocks, i * 16, X, 0, 16);
 
                 uint AA = a;
                 uint BB = b;
@@ -99,39 +90,39 @@ namespace FMCS
                 uint DD = d;
 
                 // Round 1
-                for (int i = 0; i < 16; i++)
+                for (int j = 0; j < 16; j++)
                 {
-                    a = b + LeftRotate(a + F(b, c, d) + X[i] + T[i], 7);
-                    d = a + LeftRotate(d + F(a, b, c) + X[(i + 1) % 16] + T[i + 1], 12);
-                    c = d + LeftRotate(c + F(d, a, b) + X[(i + 2) % 16] + T[i + 2], 17);
-                    b = c + LeftRotate(b + F(c, d, a) + X[(i + 3) % 16] + T[i + 3], 22);
+                    a = b + LeftRotate(a + F(b, c, d) + X[j] + T[j], 7);
+                    d = a + LeftRotate(d + F(a, b, c) + X[(j + 1) % 16] + T[j + 1], 12);
+                    c = d + LeftRotate(c + F(d, a, b) + X[(j + 2) % 16] + T[j + 2], 17);
+                    b = c + LeftRotate(b + F(c, d, a) + X[(j + 3) % 16] + T[j + 3], 22);
                 }
 
                 // Round 2
-                for (int i = 16; i < 32; i++)
+                for (int j = 16; j < 32; j++)
                 {
-                    a = b + LeftRotate(a + G(b, c, d) + X[(5 * i + 1) % 16] + T[i], 5);
-                    d = a + LeftRotate(d + G(a, b, c) + X[(5 * i + 6) % 16] + T[i + 1], 9);
-                    c = d + LeftRotate(c + G(d, a, b) + X[(5 * i + 11) % 16] + T[i + 2], 14);
-                    b = c + LeftRotate(b + G(c, d, a) + X[(5 * i + 0) % 16] + T[i + 3], 20);
+                    a = b + LeftRotate(a + G(b, c, d) + X[(5 * j + 1) % 16] + T[j], 5);
+                    d = a + LeftRotate(d + G(a, b, c) + X[(5 * j + 6) % 16] + T[j + 1], 9);
+                    c = d + LeftRotate(c + G(d, a, b) + X[(5 * j + 11) % 16] + T[j + 2], 14);
+                    b = c + LeftRotate(b + G(c, d, a) + X[(5 * j + 0) % 16] + T[j + 3], 20);
                 }
 
                 // Round 3
-                for (int i = 32; i < 48; i++)
+                for (int j = 32; j < 48; j++)
                 {
-                    a = b + LeftRotate(a + H(b, c, d) + X[(3 * i + 5) % 16] + T[i], 4);
-                    d = a + LeftRotate(d + H(a, b, c) + X[(3 * i + 8) % 16] + T[i + 1], 11);
-                    c = d + LeftRotate(c + H(d, a, b) + X[(3 * i + 11) % 16] + T[i + 2], 16);
-                    b = c + LeftRotate(b + H(c, d, a) + X[(3 * i + 14) % 16] + T[i + 3], 23);
+                    a = b + LeftRotate(a + H(b, c, d) + X[(3 * j + 5) % 16] + T[j], 4);
+                    d = a + LeftRotate(d + H(a, b, c) + X[(3 * j + 8) % 16] + T[j + 1], 11);
+                    c = d + LeftRotate(c + H(d, a, b) + X[(3 * j + 11) % 16] + T[j + 2], 16);
+                    b = c + LeftRotate(b + H(c, d, a) + X[(3 * j + 14) % 16] + T[j + 3], 23);
                 }
 
                 // Round 4
-                for (int i = 48; i < 64; i++)
+                for (int j = 48; j < 64; j++)
                 {
-                    a = b + LeftRotate(a + I(b, c, d) + X[(7 * i) % 16] + T[i], 6);
-                    d = a + LeftRotate(d + I(a, b, c) + X[(7 * i + 7) % 16] + T[i + 1], 10);
-                    c = d + LeftRotate(c + I(d, a, b) + X[(7 * i + 14) % 16] + T[i + 2], 15);
-                    b = c + LeftRotate(b + I(c, d, a) + X[(7 * i + 21) % 16] + T[i + 3], 21);
+                    a = b + LeftRotate(a + I(b, c, d) + X[(7 * j) % 16] + T[j], 6);
+                    d = a + LeftRotate(d + I(a, b, c) + X[(7 * j + 7) % 16] + T[j + 1], 10);
+                    c = d + LeftRotate(c + I(d, a, b) + X[(7 * j + 14) % 16] + T[j + 2], 15);
+                    b = c + LeftRotate(b + I(c, d, a) + X[(7 * j + 21) % 16] + T[j + 3], 21);
                 }
 
                 a += AA;
